94c59ce2beb1dab896788cce8993345a6db9d48b
Thoriqnf <Thoriqnfaizal@gmail.com>
2017-12-27 22:00:40 +0700

    init files

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import YTSearch from 'youtube-api-search'
+
+const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {videos: []}
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+      this.setState({videos})
+    })
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
